cf7061ee3ef6111ccc892c7b4019ddffec9ab9b9
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-03-15 14:09:10 +0100

    Set up the constructor and function  to connect to database

--- classes/DatabaseManager.php
+++ classes/DatabaseManager.php
@@ -15,12 +15,20 @@ class DatabaseManager
 
     public function __construct(string $host, string $user, string $password, string $dbname)
     {
-        // TODO: Set any user and password information
+        // Set any user and password information
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+        $this->dbname = $dbname;
     }
 
     public function connect(): void
     {
-        // TODO: make the connection to the database
-        $this->connection = null;
+        // Make the connection to the database
+        $dsn = "mysql:host={$this->host};dbname={$this->dbname}";
+        $this->connection = new PDO($dsn, $this->user, $this->password);
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_BOTH);
+        echo 'connected!';
     }
 }
