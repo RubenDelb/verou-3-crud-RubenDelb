c73a478704d78614435dc902e1f8dcb6a85e56f5
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-03-17 15:48:54 +0100

    Add page show.php with basic information

---
+++ show.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Title: <?= $fetch['name']; ?></h1>
+    <br>
+    <h4>Description: <?= $fetch['description']; ?></h4>
+    <br>
+    <h6>Genre: <?= $fetch['genre']; ?></h6>
+</body>
+</html>
